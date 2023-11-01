fb78f38090a2b4428fc0e61081b55402707500ad
Documenter.jl <documenter@juliadocs.github.io>
2023-11-01 20:31:56 +0000

    build based on 5bc4995

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Homework6","category":"page"},{"location":"#Homework6","page":"Home","title":"Homework6","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Homework6.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Homework6]","category":"page"}]
+}
